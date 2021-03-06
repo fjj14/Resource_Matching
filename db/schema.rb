5a84efff48ecf45b0106d441c0bcb306a7ea423c
Fatumata Jalloh <fatumatajalloh014@gmail.com>
2021-03-06 17:27:09 -0500

    Updated the database to have first and last name

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_28_032111) do
+ActiveRecord::Schema.define(version: 2021_03_06_222344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.string "password"
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first"
+    t.string "last"
   end
 
 end
