2c3ac380080b1e29d891274e7ac067af6512f93a
yaldamauj <yaldamauj@brandeis.edu>
2021-03-07 10:10:30 -0500

    created a sign in page

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+    validates :first, length: { minimum: 2 }, 
+    presence: true
+    validates :last, length: { minimum: 2 }, 
+    presence: true
+    validates :email, length: { minimum: 3 }, 
+    presence: true
+    validates :email, uniqueness: true
+    validates :password, length: {minimum: 8}, presence: true;
+    validates :username, length: {minimum: 3}, uniqueness: true; 
+end
