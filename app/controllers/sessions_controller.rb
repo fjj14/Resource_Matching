dbe5c0ec718c2abca9e16d0af73c1b8c17842da0
yaldamauj <yaldamauj@brandeis.edu>
2021-03-07 11:24:07 -0500

    create login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create 
-    
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else 
+      flash[:danger] = 'Incorrect email/password. Please try again!'
+      render 'new'
+    end
+  end
+
+  def destroy
   end
 end
