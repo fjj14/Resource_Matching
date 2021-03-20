c5b07de1e99a5b9ea3061d7f81a7576c690b4d4d
Fatumata Jalloh <fatumatajalloh014@gmail.com>
2021-03-19 23:29:48 -0400

    Fixed Columns error

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,11 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+//= require jquery
+//= require jquery-ui
+//= require popper
+//= require bootstrap-sprockets
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
