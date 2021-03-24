9bad65552c349818243f77abf82691e8ebe6be6e
Fatumata Jalloh <fatumatajalloh014@gmail.com>
2021-03-24 00:21:33 -0400

    proc

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.delete_all
-Product.delete_all
+User.destroy_all
+Product.destroy_all
 
 20.times do
     User.create(first: Faker::Name.first_name, last:Faker::Name.last_name, email: Faker::Internet.username+"@gmail.com", username: Faker::Internet.username, password: Faker::Internet.password) #password: Faker::Internet.password
