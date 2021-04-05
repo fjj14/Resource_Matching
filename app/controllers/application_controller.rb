7f13310a0a10a8b22de5da802e7b042adeeeed2c
Fatumata Jalloh <fatumatajalloh014@gmail.com>
2021-04-05 10:54:24 -0400

    added current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
-    
+    helper_method :current_user 
     rescue_from CanCan::AccessDenied do |exception|
         redirect_to main_app.login_path, alert: exception.message
     end
 
     include SessionsHelper
+
+    def current_user  
+        return unless session[:user_id]  
+        @current_user ||= User.find(session[:user_id])  
+      end  
 end
