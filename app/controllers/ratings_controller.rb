1f727021cfd4ca66de846f8790caa8199f48a8ca
sophia <sophiaw@brandeis.edu>
2021-04-09 15:38:11 -0400

    rating update

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,5 +1,6 @@
 class RatingsController < ApplicationController
     def new 
+        @rating = Rating.new
     end
     def create
         @rating = Rating.new(rating_params)
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   resources :sessions
   resources :products
   resources :users
+  resources :ratings
   resources :conversations do
     resources :messages
+
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#new'
