ead6bcc8e67a760167f2d2e6dedfdbd14aa9f2c7
Fatumata Jalloh <fatumatajalloh014@gmail.com>
2021-04-18 16:27:18 -0400

    client id

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
         redirect_uri = stripe_connect_url
         client_id = ENV["STRIPE_CLIENT_ID"]
       
-       "https://connect.stripe.com/express/oauth/authorize?redirect_uri=https://buydeis.herokuapp.com/welcome&client_id=#{ENV['STRIPE_CLIENT_ID']}&state=#{STATE_VALUE}"
+       "https://connect.stripe.com/express/oauth/authorize?redirect_uri=https://buydeis.herokuapp.com/welcome&client_id=#{client_id}&state=#{STATE_VALUE}"
        
       end
 end
