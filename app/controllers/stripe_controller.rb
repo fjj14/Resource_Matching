024b269ef0449449cf5ca719e0b17420bd617a0a
Fatumata Jalloh <fatumatajalloh014@gmail.com>
2021-04-18 12:08:47 -0400

    Stripe connect

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -2,7 +2,7 @@ class StripeController < ApplicationController
     def connect
         response = HTTParty.post("https://connect.stripe.com/oauth/token",
         query: {
-          client_secret: ENV["STRIPE_SECRET_KEY"],
+          client_secret: ENV.fetch('STRIPE_CLIENT_SECRET'),
           code: params[:code],
           grant_type: "authorization_code"
         }
@@ -13,7 +13,7 @@ class StripeController < ApplicationController
         else
             stripe_user_id = response.parsed_response["stripe_user_id"]
             current_user.stripe_user_id = stripe_user_id
-
+            current_user.save
             redirect_to mypage_path,
             notice: 'User successfully connected with Stripe!'
         end
