ea37fc529d2ed23e09de563207ae9b5dad8219cd
Fatumata Jalloh <fatumatajalloh014@gmail.com>
2021-04-17 22:55:34 -0400

    Stripe id

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -12,7 +12,7 @@ class StripeController < ApplicationController
             notice: response.parsed_response["error_description"]
         else
             stripe_user_id = response.parsed_response["stripe_user_id"]
-            current_user.update_attribute(:stripe_user_id, stripe_user_id)
+            current_user.stripe_user_id = stripe_user_id)
 
             redirect_to mypage_path,
             notice: 'User successfully connected with Stripe!'
