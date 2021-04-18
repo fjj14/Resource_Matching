f51d2f25cdcee10f0e5b265a055a2a796eb21608
Fatumata Jalloh <fatumatajalloh014@gmail.com>
2021-04-18 12:18:50 -0400

    Stripe connect

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
         redirect_uri = stripe_connect_url
         client_id = ENV["STRIPE_CLIENT_ID"]
       
-        "#{stripe_url}?redirect_uri=#{redirect_uri}&client_id=#{client_id}"
+        "#{stripe_url}?redirect_uri=#{redirect_uri}&client_id=#{client_id}&response_type=code&scope=read_write&stripe_user[country]=US"
        
       end
 end
