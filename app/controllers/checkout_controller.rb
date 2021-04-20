e188d4c502386bc865fdbf929fb8dd1aa481fdec
Fatumata Jalloh <fatumatajalloh014@gmail.com>
2021-04-19 20:59:45 -0400

    stripe

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -5,7 +5,8 @@ class CheckoutController < ApplicationController
     end
     def create 
         @product = Product.find(params[:id])
-        @product.update_column(:buyer_id, current_user.id)
+        @product.update!(buyer_id: current_user.id)
+        
         if @product.nil?
             redirect_to welcome_path
             return
@@ -20,7 +21,8 @@ class CheckoutController < ApplicationController
                 currency: 'usd',
                 quantity: 1
             }],
-            success_url: checkout_success_url(product: @product.id) ,
+           
+            success_url: checkout_success_url+"?session_id={CHECKOUT_SESSION_ID}&product=#{@product.id}",
             cancel_url: checkout_cancel_url(product: @product.id)
         )
 
@@ -30,10 +32,17 @@ class CheckoutController < ApplicationController
     end 
     def success 
        #call account transfer
-       # @session = Stripe::Checkout::Session.retrieve(params[:session_id])
-       # @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
+        @session = Stripe::Checkout::Session.retrieve(params[:session_id])
+        @payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent).application_fee_amount.to_f
         @product = Product.where(id: params[:product]).first
-       
+        seller = User.where(id: @product.user_id).first
+        transfer = Stripe::Transfer.create({
+            amount: (((@product.price) *100).to_i)-((@payment_intent*100).to_i),
+            currency: 'usd',
+            destination: seller.stripe_user_id,
+           
+          })
+          
     end
     def cancel 
         @product = Product.where(id: params[:product]).first
