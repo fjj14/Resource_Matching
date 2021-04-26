648e4e3308f328eeca0e28228d5ecb2c08e7ff26
Fatumata Jalloh <fatumatajalloh014@gmail.com>
2021-04-26 17:54:03 -0400

    undo

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -4,6 +4,10 @@ if Rails.env == 'production'
     options = { hosts: url, retry_on_failure: true, transport_options: transport_options }
     Searchkick.client = Elasticsearch::Client.new(options)
 end
-if Rails.env == 'development'
-  Searchkick.client = Elasticsearch::Client.new(hosts: ["localhost:3000"], retry_on_failure: true, transport_options: {request: {timeout: 250}})
+if Rails.env.development?
+  Searchkick.client =
+    Elasticsearch::Client.new(
+      url: '127.0.0.1:9200',
+      transport_options: {request: {timeout: 200}}
+    )
 end
