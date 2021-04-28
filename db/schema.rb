49c2bc97032e626006a549fc45f4cb62de8b958f
yaldamauj <yaldamauj@brandeis.edu>
2021-04-27 23:38:20 -0400

    my page frontend

--- db/schema.rb
+++ db/schema.rb
@@ -159,9 +159,7 @@ ActiveRecord::Schema.define(version: 2021_04_26_162730) do
     t.integer "followed_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["followed_id"], name: "index_relationships_on_followed_id"
     t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
-    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "taggings", id: :serial, force: :cascade do |t|
