1faeecf4142db69c191beb2371e8eb54a715f2db
Fatumata Jalloh <fatumatajalloh014@gmail.com>
2021-04-27 20:38:00 -0400

    filtering

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,13 +2,14 @@ class Cart < ApplicationRecord
     has_many :products
     has_many :line_items, dependent: :destroy
     belongs_to :user, optional: true
+    
     def add_item(product_id, cart)
         k = Product.find(product_id)
         if !cart.include? (k.id)
             cart.push(k.id)    
         end
         redirect_to cart
-     end
+    end
 
     def add_product(product)
         current_item = line_items.find_by(product_id: product.id)
