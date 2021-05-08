ff86d189dba8e8c34a676c2508f6396272738087
Fatumata Jalloh <fatumatajalloh014@gmail.com>
2021-05-08 17:43:59 -0400

    stripe id for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,9 @@ Rating.destroy_all
 User.destroy_all
 Product.destroy_all
 Category.destroy_all
+
 20.times do
-    User.create(first: Faker::Name.first_name, last:Faker::Name.last_name, email: Faker::Internet.username+"@gmail.com", username: Faker::Internet.username, password: Faker::Internet.password, venmo_id: Faker::Internet.username) #password: Faker::Internet.password
+    User.create(first: Faker::Name.first_name, last:Faker::Name.last_name, email: Faker::Internet.username+"@gmail.com", username: Faker::Internet.username, password: Faker::Internet.password, stripe_user_id: rand(1000), venmo_id: Faker::Internet.username) #password: Faker::Internet.password
 end
 Condition.create(name: "New")
 Condition.create(name: "Good")
